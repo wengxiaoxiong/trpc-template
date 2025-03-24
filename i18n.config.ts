c2e1b68d3995ef2d040ee67c54fac7dec6e4b7a9
wengxiaoxiong <owner@wengxiaoxiong.com>
2025-03-24 21:33:48 +0800

    chore: 修复打包bug

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,13 +1,7 @@
-import { getRequestConfig } from 'next-i18next/serverConfig';
+
 
 export const i18nConfig = {
   defaultLocale: 'zh',
   locales: ['zh', 'en', 'ja'],
   localeDetection: true,
 }
-
-export default async function getConfig() {
-  return getRequestConfig({
-    ...i18nConfig,
-  });
-} 
